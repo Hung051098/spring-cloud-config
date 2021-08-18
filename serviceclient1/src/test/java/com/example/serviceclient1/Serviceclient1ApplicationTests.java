baa9a166b567d07ce587f9b32c16b063aa5200ae
Hung051098 <39266629+Hung051098@users.noreply.github.com>
2021-08-18 11:22:29 +0700

    init

---
+++ serviceclient1/src/test/java/com/example/serviceclient1/Serviceclient1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviceclient1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Serviceclient1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
